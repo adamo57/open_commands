de6738b1aaf750084b893e0aee73caf898e3b936
adamo57 <adamouellette57@gmail.com>
2017-04-27 10:42:39 -0400

    Added open command for BIRTA

---
+++ open_birta/install.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+
+chmod +x ./open_birta.sh
+
+cp ./open_birta.sh /usr/local/bin
---
+++ open_birta/open_birta.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ `uname -s` != "Darwin" ] && return
+
+function tab () {
+    local cdto="$PWD"
+    local args="$@"
+
+    if [ -d "$1" ]; then
+        cdto=`cd "$1"; pwd`
+        args="${@:2}"
+    fi
+
+    osascript &>/dev/null <<EOF
+        tell application "System Events"
+            tell process "Terminal" to keystroke "t" using command down
+        end tell
+        tell application "Terminal"
+            activate
+            do script with command "cd \"$cdto\"; $args" in selected tab of the front window
+        end tell
+EOF
+}
+
+tab ~/code/birta 
+clear
+tab ~/code/birta "docker-compose up"
+clear
+tab ~/code/birta "docker run -it -w /var/www -v "$PWD:/var/www" -v "$HOME/.ssh:/root/.ssh" businessinsider/birta:build /bin/bash"
